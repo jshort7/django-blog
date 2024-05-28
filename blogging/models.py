6dd3a7710ad55ee7b68b5a44f38cf9b87f57154b
Josh Short <jshort7@uw.edu>
2024-05-27 21:24:04 -0500

    Act 7

--- blogging/models.py
+++ blogging/models.py
@@ -1,7 +1,6 @@
 from django.db import models
-
-# Create your models here.
 from django.contrib.auth.models import User
+
 
 class Post(models.Model):
 
@@ -14,3 +13,17 @@ class Post(models.Model):
 
     def __str__(self):
         return self.title
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=128)
+    description = models.TextField(blank=True)
+    posts = models.ManyToManyField(Post, blank=True, related_name='categories')
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name_plural = 'Categories'
+
+
