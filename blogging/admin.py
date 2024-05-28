a6673b986006aa30d80e5a395be77048b4c9a0a0
Josh Short <jshort7@uw.edu>
2024-05-27 21:31:25 -0500

    assign 7

--- blogging/admin.py
+++ blogging/admin.py
@@ -1,6 +1,20 @@
 from django.contrib import admin
 from blogging.models import Post, Category
-# Register your models here.
-admin.site.register(Post)
-admin.site.register(Category)
+
+# This was not fun.....
+# Create cat in post
+class CategoryInPostInline(admin.TabularInline):
+    model = Category.posts.through
+
+# add in line
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    inlines = [
+        CategoryInPostInline,
+    ]
+
+# add admin cat
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    exclude = ('posts',)
 
